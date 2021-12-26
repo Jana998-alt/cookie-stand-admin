b7d6dd3b497efad20e4b9488183c1978390c5219
Jana Abdelkader <janaosama1300@gmail.com>
2021-12-26 14:17:50 +0200

    added login form

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import CookieStandAdmin from '../components/CookieStandAdmin.js'
+import LoginForm from '../components/LoginForm.js'
 import Footer from '../components/Footer.js'
+import axios from 'axios'
+
+
+
+
+const tokenUrl = 'http://127.0.0.1:8000/api/token/';
+const refreshToken = 'http://127.0.0.1:8000/api/token/refresh/';
+
+
 
 export default function Home() {
 
+  const [token, setToken] = useState("")
+  const [refreshToken, setRefreshToken] = useState("")
+
+  async function getToken(userCredentials) {
+    const token_recieved = await axios.post(tokenUrl, userCredentials)
+
+    setToken(token_recieved.data.access)
+    setRefreshToken(token_recieved.data.refresh)
+  }
+
+  if (token === "") {
+    return (
+
+      <LoginForm getToken={getToken}/>
+
+    )
+  }
 
 
-  return (
+  else {
+    return (
 
-      <CookieStandAdmin/>
+      <CookieStandAdmin />
 
-  )
+    )
+  }
 
 }
