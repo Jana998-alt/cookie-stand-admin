975378968eddc1049bad1c3d6a3cc2d94987fa46
Jana Abdelkader <janaosama1300@gmail.com>
2021-12-31 17:26:46 +0200

    styling

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -11,29 +11,31 @@ export default function CreateForm(props) {
                 <br />
                 <div className="flex-auto w-full ">
                     <label>location</label>
-                    <input type="text" className="flex-auto w-full" name='location'></input>
+                    <input type="text" className="flex-auto w-7/12 rounded-sm" name='location'></input>
+
+                    <button className="rounded-sm p-2 m-4 bg-emerald-400 w-1/4">Create</button>
                 </div>
 
                 <div className="flex ">
-                    <div className="p-2 bg-emerald-100 m-4">
+                    <div className="p-2 m-4">
                         <label>Minimum Customers Per Hour</label>
                         <br />
-                        <input type="number" className="flex-auto" name='min'></input>
+                        <input type="number" className="flex-auto m-3 p-2 rounded-sm" name='min'></input>
                     </div>
 
-                    <div className="p-2 bg-emerald-100 m-4">
+                    <div className="p-2 m-4">
                         <label>Maximum Customers Per Hour</label>
                         <br />
-                        <input type="number" className="flex-auto" name='max'></input>
+                        <input type="number" className="flex-auto m-3 p-2 rounded-sm" name='max'></input>
                     </div>
 
-                    <div className="p-2 bg-emerald-100 m-4">
+                    <div className=" p-2 m-4">
                         <label>Average Cookies Per Sale</label>
                         <br />
-                        <input type="number" className="flex-auto" name='avg' step="any"></input>
+                        <input type="number" className="flex-auto m-3 p-2 rounded-sm" name='avg' step="any"></input>
                     </div>
 
-                    <button className="p-2 m-4 bg-emerald-400 w-1/4">Create</button>
+
                 </div>
 
             </form>
