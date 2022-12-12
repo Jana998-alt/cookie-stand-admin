78d42d2f1f6968f3f67711142442c294ec150199
Jana Abdelkader <janaosama1300@gmail.com>
2022-12-12 13:20:02 +0300

    added a comment

--- components/IndexPage.js
+++ components/IndexPage.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react'
-import Head from 'next/head'
+import React, { Component } from "react";
+import Head from "next/head";
 
-export default function IndexPage(){
-
-        return (
-                <div>
-            <Head>
-                <title>Cookie Stand Admin</title>
-            </Head>
-                
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SX60M6MWJD"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-SX60M6MWJD');
-</script>
-        </div>
-
-
-        )
-
+export default function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Cookie Stand Admin</title>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-SX60M6MWJD"
+        ></Script>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)}
+gtag('js', new Date());
+gtag('config', 'G-SX60M6MWJD');`,
+          }}
+        ></Script>
+      </Head>
+    </div>
+  );
 }
-
